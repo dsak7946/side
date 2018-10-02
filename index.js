4525a14e0ccb8a38ee57c735c876eea62f0df1a7
dsak7946 <dsak7946@gmail.com>
2018-10-03 05:06:23 +0800

    ok

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var linebot = require('linebot');
 var express = require('express');
 var google = require('googleapis');
 var googleAuth = require('google-auth-library');
+
 var bot = linebot({
     channelId: '1611184250',
     channelSecret: 'fc0dde92ef9e9b182bc526a240c18346',
@@ -10,16 +11,16 @@ var bot = linebot({
 	});
  
 //底下輸入client_secret.json檔案的內容
-var myClientSecret="installed":{"client_id":"583651028506-fvmng0ph5iesllm8jifa3s7j0lorvvof.apps.googleusercontent.com","project_id":"coral-smoke-218120","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://www.googleapis.com/oauth2/v3/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"gBtSIM6stlySHqzQIbVXuMFI","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}
+var myClientSecret={"installed":{"client_id":"583651028506-fvmng0ph5iesllm8jifa3s7j0lorvvof.apps.googleusercontent.com","project_id":"coral-smoke-218120","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://www.googleapis.com/oauth2/v3/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"gBtSIM6stlySHqzQIbVXuMFI","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}
 
 var auth = new googleAuth();
 var oauth2Client = new auth.OAuth2(myClientSecret.installed.client_id,myClientSecret.installed.client_secret, myClientSecret.installed.redirect_uris[0]);
 
 //底下輸入sheetsapi.json檔案的內容
-oauth2Client.credentials ="access_token":"ya29.GlsqBjYS4KILSI9k5ViN_y-IDRGV8rzsT_9SHU3EU9eM4ciQPKooVJPMo6RmmB_iGg3tZiduKNmwYho5uwQGIfHVRzl52sgeKlLJkHvKHag8ytLIZhKoEwzS70xa","refresh_token":"1/ebYF3Vpbjgv-1mcDwl8YqI7azXLZPcqK3cajm7tKWKiEQWHPENrYj6-A2K3vr1YU","scope":"https://www.googleapis.com/auth/spreadsheets","token_type":"Bearer","expiry_date":1538510289455
+oauth2Client.credentials ={"access_token":"ya29.GlsqBjYS4KILSI9k5ViN_y-IDRGV8rzsT_9SHU3EU9eM4ciQPKooVJPMo6RmmB_iGg3tZiduKNmwYho5uwQGIfHVRzl52sgeKlLJkHvKHag8ytLIZhKoEwzS70xa","refresh_token":"1/ebYF3Vpbjgv-1mcDwl8YqI7azXLZPcqK3cajm7tKWKiEQWHPENrYj6-A2K3vr1YU","scope":"https://www.googleapis.com/auth/spreadsheets","token_type":"Bearer","expiry_date":1538510289455}
 
 //試算表的ID，引號不能刪掉
-var mySheetId='1PKjYBeg1C85_24_oqs8We7gaT8Hx5j7xCR7QdlIXn54/edit#gid=1143448686';
+var mySheetId='1PKjYBeg1C85_24_oqs8We7gaT8Hx5j7xCR7QdlIXn54';
 
 var myQuestions=[];
 var users=[];

96dadcbb6efa460b9bc0c3f5934c11942f19ad2f
dsak7946 <dsak7946@gmail.com>
2018-10-11 04:30:03 +0800

    'ok'

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ bot.on('message', function(event) {
          if (myStep==(totalSteps-1))
             sendMessage(event,myQuestions[1][myStep]);
          else
-            sendMessage(event,myQuestions[1][myStep]+1),
+            sendMessage(event,myQuestions[1][myStep+1]),
 			sendMessage(event,myQuestions[0][myStep+2]);
          users[myId].replies[myStep+1]=event.message.text;
       }

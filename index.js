245ed106ab03127e4c5f0d87740854e5c5cdbebb
dsak7946 <dsak7946@gmail.com>
2018-10-11 02:56:55 +0800

    'ok'

--- index.js
+++ index.js
@@ -10,123 +10,25 @@ var bot = linebot({
     channelAccessToken: 'fo/507dPjAsjw/gAjXcas2aKo94L9l5QOLrDqTkQ6fcsy5lDv4uRGAGHd0ck8DAumYuGVRYO9pNMJXWrcohw/2KnyeD0XJ1y2fW7fdgfpzmd5ChC5KuAV9REP9kFNlFubCii5jKuSVY81oDn3KTvRwdB04t89/1O/w1cDnyilFU='
 	});
  
-//底下輸入client_secret.json檔案的內容
-var myClientSecret={"installed":{"client_id":"583651028506-fvmng0ph5iesllm8jifa3s7j0lorvvof.apps.googleusercontent.com","project_id":"coral-smoke-218120","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://www.googleapis.com/oauth2/v3/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"gBtSIM6stlySHqzQIbVXuMFI","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}
-
-var auth = new googleAuth();
-var oauth2Client = new auth.OAuth2(myClientSecret.installed.client_id,myClientSecret.installed.client_secret, myClientSecret.installed.redirect_uris[0]);
-
-//底下輸入sheetsapi.json檔案的內容
-oauth2Client.credentials ={"access_token":"ya29.GlsyBqOC4eCB5NPDgl9nOYJhPLFGuJDJbMA2INQF9Umh-ZLYXWtb6QTFUGN-DdAxuQM5Bgi36IYLKV9IrNiMql1zWQcJYansuRGUagfTna5xBCckyXmXM2b5CgQo","refresh_token":"1/cSQDIaLXFErKVfdT2-DH0ADjMSIU1L2QKFfgLWZpnKk","scope":"https://www.googleapis.com/auth/spreadsheets","token_type":"Bearer","expiry_date":1539199944860}
-
-//試算表的ID，引號不能刪掉
-var mySheetId='1xI2UxdRH0AJd2h5t0LcW66SjLOrZ3RiBcRMz1zEzd64';
-
-var myQuestions=[];
-var users=[];
-var totalSteps=0;
-var myReplies=[];
-
-//程式啟動後會去讀取試算表內的問題
-getQuestions();
-
-
-//這是讀取問題的函式
-function getQuestions() {
-  var sheets = google.sheets('v4');
-  sheets.spreadsheets.values.get({
-     auth: oauth2Client,
-     spreadsheetId: mySheetId,
-     range:encodeURI('問題'),
-  }, function(err, response) {
-     if (err) {
-        console.log('讀取問題檔的API產生問題：' + err);
-        return;
-     }
-     var rows = response.values;
-     if (rows.length == 0) {
-        console.log('No data found.');
-     } else {
-       myQuestions=rows;
-       totalSteps=myQuestions[0].length;
-       console.log('要問的問題已下載完畢！');
-     }
-  });
-}
-
-//這是將取得的資料儲存進試算表的函式
-function appendMyRow(userId) {
-   var request = {
-      auth: oauth2Client,
-      spreadsheetId: mySheetId,
-      range:encodeURI('表單回應 1'),
-      insertDataOption: 'INSERT_ROWS',
-      valueInputOption: 'RAW',
-      resource: {
-        "values": [
-          users[userId].replies
-        ]
-      }
-   };
-   var sheets = google.sheets('v4');
-   sheets.spreadsheets.values.append(request, function(err, response) {
-      if (err) {
-         console.log('The API returned an error: ' + err);
-         return;
-      }
-   });
-}
-
-//LineBot收到user的文字訊息時的處理函式
 bot.on('message', function(event) {
-   if (event.message.type === 'text') {
-      var myId=event.source.userId;
-      if (users[myId]==undefined){
-         users[myId]=[];
-         users[myId].userId=myId;
-         users[myId].step=-1;
-         users[myId].replies=[];
-      }
-      var myStep=users[myId].step;
-      if (myStep===-1)
-         sendMessage(event,myQuestions[0][0]);
-      else{
-         if (myStep==(totalSteps-1))
-            sendMessage(event,myQuestions[1][myStep]);
-         else
-            sendMessage(event,myQuestions[1][myStep]+'\n'+myQuestions[0][myStep+1]);
-         users[myId].replies[myStep+1]=event.message.text;
-      }
-      myStep++;
-      users[myId].step=myStep;
-      if (myStep>=totalSteps){
-         myStep=-1;
-         users[myId].step=myStep;
-         users[myId].replies[0]=new Date();
-         appendMyRow(myId);
-      }
-   }
+  if (event.message.type = 'text') {
+    var msg = event.message.text;
+  //收到文字訊息時，直接把收到的訊息傳回去
+    event.reply(msg).then(function(data) {
+      // 傳送訊息成功時，可在此寫程式碼 
+      console.log(msg);
+    }).catch(function(error) {
+      // 傳送訊息失敗時，可在此寫程式碼 
+      console.log('錯誤產生，錯誤碼：'+error);
+    });
+  }
 });
-
-
-//這是發送訊息給user的函式
-function sendMessage(eve,msg){
-   eve.reply(msg).then(function(data) {
-      // success 
-      return true;
-   }).catch(function(error) {
-      // error 
-      return false;
-   });
-}
-
 
 const app = express();
 const linebotParser = bot.parser();
 app.post('/', linebotParser);
 
-//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
 var server = app.listen(process.env.PORT || 8080, function() {
   var port = server.address().port;
-  console.log("App now running on port", port);
+  console.log('目前的port是', port);
 });

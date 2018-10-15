4d93be4b7a05c08ebb695ab18b3dd2788df483e0
dsak7946 <dsak7946@gmail.com>
2018-10-16 03:47:54 +0800

    ok

--- index.js
+++ index.js
@@ -145,5 +145,5 @@ app.post('/', linebotParser);
 
 var server = app.listen(process.env.PORT || 8080, function() {
   var port = server.address().port;
-  console.log('目前的port是', port);
+  console.log('App now running on port', port);
 });

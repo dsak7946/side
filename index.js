9fdba42b866f0ba408f282eea99a43f52c7ad77e
dsak7946 <dsak7946@gmail.com>
2018-10-23 03:23:42 +0800

    ok

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ function appendMyRow(userId) {
 //LineBot收到user的文字訊息時的處理函式
 bot.on('message', function(event) {
 	let msg = '我要報名';
-   if (event.message.type === msg) {
+   if (event.message.type === 'text') {
       var myId=event.source.userId;
       if (users[myId]==undefined){
          users[myId]=[];
@@ -286,5 +286,5 @@ app.post('/', linebotParser);
 var server = app.listen(process.env.PORT || 8080, function() {
   var port = server.address().port;
   console.log('目前的port是', port);
-  console.log(typeof(type));
+  console.log(typeof('text'));
 });

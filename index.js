a8c9f9e4fe7083cb16754e79bb16aecb4bb79c85
dsak7946 <dsak7946@gmail.com>
2018-11-01 14:12:45 +0800

    skr

--- index.js
+++ index.js
@@ -176,8 +176,9 @@ const app = express();
 app.post('/', bot.parser());
 app.get('/', function (req, res) {
     res.sendfile(__dirname + '/views/index.html');
+    console.log(res.sendfile);
 });
-console.log(res.sendfile);
+
 app.post('/data', [bodyParser.json(), bodyParser.urlencoded({extended: false})], function (req, res) {
     // console.log(req.body);
     let reqJson = req.body;

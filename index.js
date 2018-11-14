b1beb02d3805a06ad0efb47333808618da19d2f2
dsak7946 <dsak7946@gmail.com>
2018-11-15 03:43:27 +0800

    add text

--- index.js
+++ index.js
@@ -165,13 +165,11 @@ bot.on('message', function (event) {
             return;
         }
 		else if(requestMessage.indexOf("移除使用者") >= 0){
-			 let user = fireBaseCollector.bind(lineid, bindId);
 			 let removeUser = requestMessage.replace("移除使用者", "");
-			 if (user){
 			 broadcast("user", {TYPE: "UPDATE_USER"});
              event.reply(["移除成功!", "感謝您使用該系統"]);
 			 fireBaseCollector.removeUser(reqJson.USER.BIND);
-			 }
+			 
 		}
         fireBaseCollector.getResponeMessage(requestMessage, function (respone) {
             if (respone) {

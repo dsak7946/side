fed4eab63eb2d35e297a28b6d3ed91b4e025e476
dsak7946 <43715615+dsak7946@users.noreply.github.com>
2018-12-12 00:27:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,210 +7,203 @@ const fireBaseCollector = require('./FireBaseCollector.js');
 const path = require('path');
 const clientSocket = {};
 const io = require('socket.io');
+
 _getWeather();
+
 //LINE deploy_info
 var bot = linebot({
     channelId: '1611184250',
     channelSecret: 'fc0dde92ef9e9b182bc526a240c18346',
     channelAccessToken: 'fo/507dPjAsjw/gAjXcas2aKo94L9l5QOLrDqTkQ6fcsy5lDv4uRGAGHd0ck8DAumYuGVRYO9pNMJXWrcohw/2KnyeD0XJ1y2fW7fdgfpzmd5ChC5KuAV9REP9kFNlFubCii5jKuSVY81oDn3KTvRwdB04t89/1O/w1cDnyilFU='
 });
+
+var go = '正常';
 var wt = [];
+var ps = [];
 var timer;
 var buttonsImageURL_N = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Northern_Taiwan_official_determined.svg/240px-Northern_Taiwan_official_determined.svg.png';
 var buttonsImageURL_M = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Central_Taiwan_official_determined.svg/240px-Central_Taiwan_official_determined.svg.png';
 var buttonsImageURL_S = 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Southern_Taiwan_official_determined.svg/240px-Southern_Taiwan_official_determined.svg.png';
 var buttonsImageURL_E = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Eastern_Taiwan_official_determined.svg/240px-Eastern_Taiwan_official_determined.svg.png';
 var buttonsImageURL_F = 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Fujian_Province_in_Taiwan_%28special_marker%29.svg/240px-Fujian_Province_in_Taiwan_%28special_marker%29.svg.png';
-
+var userlist = (fireBaseCollector.getUsers());
+var JustGps = {
+  "type": "location",
+  "title": "景文科技大學",
+  "address": "231新北市新店區安忠路99號",
+  "latitude": 24.9534475,
+  "longitude": 121.5070476
+}
 bot.on('message', function (event) {
+  function _getStatus() {
+    // clearTimeout(timer);
+    getJSON('https://XXX.XXX.airXb.io/api/v1/sensors/sensorD/2018-12-09/helmet', function (error, response, endstatus) {
+      var output = JSON.stringify(response.results);
+      console.log(output);
+      var locations = response.results['0000000012345691']['locations'];//取location's Data
+      var locationlastOne = locations[locations.length - 1];//最新一筆資料
+      // console.log(locations[0].lat, locations[0].lng);//第一筆資料
+      var latt = locationlastOne.lat;
+      var lngg = locationlastOne.lng;
+      // console.log(latt, lngg);//最新一筆資料 (北緯,東經)
 
-  //       "你是誰": function (event) {
-  //       event.reply(event.replyToken, "我是ㄐ器人");
-  //       },
-  //       "當前人數": function (event) {
-  //       event.reply(event.replyToken, "目前有" + (people_num) + "人");
-  //       },
-  //       "安全注意事項": function (event) {
-  //       event.reply(event.replyToken), "1.注意掉落物\n2.留意腳邊障礙物\n3.配戴安全帽、安全護目鏡及安全手套\n4.物品不可任意堆置、通道要保持流通"}
-  //       }
+
+      var Status = response.results['0000000012345691']['angles'];
+      var StatuslastOne = Status[Status.length - 1];
+      var anglee = StatuslastOne.angle;
+      var statuss = StatuslastOne.status;
+      // console.log(anglee, statuss);
+      if (statuss == "00") {
+        go = '正常';
+      }
+      else {
+        go = '異常';
+      }
+
+      var Temperatures = response.results['0000000012345691']['temperatures'];
+      var TemperatureslastOne = Temperatures[Temperatures.length - 1];
+      var temperaturee = TemperatureslastOne.temperature;
+      var timee = TemperatureslastOne.time;
+      var localtime = new Date(timee);
+      localtime.setHours(localtime.getHours() + 8);
+      var localtimee = localtime.toLocaleString();
+      console.log(temperaturee, timee);
+      endstatus = ('人員編號:0000000012345691' + '\n' + '定位-北緯,東經 : ' + (latt) + ', ' + (lngg) + '\n' + '體溫 : ' + (temperaturee) + '°Ｃ' + '\n' + '傾角 : ' + (anglee) + '\n' + '狀態 : ' + (go) + '\n' + '更新時間 : ' + (localtimee));//+ '\n' + 'time : ' + (timee)
+      bot.push(lineid, endstatus);
+      console.log(endstatus);
+      return endstatus;
+      // return endstatus;
+    });
+  }
   let requestMessage = event.message.text;
   let lineid = event.source.userId;
-  var er = 12;
-  var vv = 344;
-  var yh = 445566;
+  var checkin = new Boolean(false);
   var replyMsg = '';
-
+  var aaa = 0;
+  var vv = '';
   console.log(event);
 
-  // if (requestMessage=="我要註冊") {
-  //   bot.push(lineid,"您的ID:"+lineid);
-
-  //   // let bindId = requestMessage.replace("綁定", "");
-  //   // let user = fireBaseCollector.bind(lineid, bindId);
-  //   // if (user) {
-  //   //   broadcast("user", { TYPE: "UPDATE_USER" });
-  //   //   event.reply(["註冊成功!", "歡迎 " + user.NAME + " 使用該系統"]);
-  //   //   let data = find(unknowjoinList, "LINEID", lineid);
-  //   //   if (data) {
-  //   //     unknowjoinList.splice(data[1], 1);
-  //   //     fireBaseCollector.userEnter(lineid, data.TIME);
-  //   //     let d = {
-  //   //       LINEID: lineid,
-  //   //       NAME: user.NAME,
-  //   //       NUMBER: user.NUMBER,
-  //   //       TIME: data.TIME
-  //   //     };
-  //   //     joinList.push(d);
-  //   //     broadcast("online", { TYPE: "REMOVE", LINEID: lineid })
-  //   //     broadcast("online", { TYPE: "ADD", UNKNOWN: false, DATA: d })
-  //   //   }
-  //   // } else {
-  //   //   event.reply("該綁定碼不存在或已經被綁定");
-
-  //   return;
-  // } 
-  event.source.profile().then(function (profile) {
+  event.source.profile().then(function (profile) {   //Loading Firebase message json for reply.
     user = profile.displayName;
-    fireBaseCollector.getResponeMessage(requestMessage, function (respone) {
-      if (respone) {
-        bot.push(lineid, respone);
-      } else {
-        if (requestMessage == "注意事項") {
-          bot.push(lineid, "1.注意掉落物\n2.留意腳邊障礙物\n3.配戴安全帽、安全護目鏡及安全手套\n4.物品不可任意堆置、通道要保持流通");
-        }
-        else if (requestMessage == "當前人數") {
-          bot.push(lineid, "目前有" + (people_num) + "人");
-        }
-        else if (requestMessage == "我要註冊") {
-          bot.push(lineid, "您的ID:" + lineid);
-          event.reply(["請輸入使用者名稱:"]);
-          // bot.push(lineid,"請輸入使用者名稱:",);
-          var useridd = event.message.text;
-          bot.push(lineid, "您輸入的使用者名稱為：" + (useridd));
-          fireBaseCollector.addUser(useridd, vv, lineid);
-        }
-        else if (requestMessage == "天氣資訊") {
-          bot.push(
-            lineid,
-            {
-              type: 'template',
-              altText: '天氣資訊清單',
-              template: {
-                // type: 'buttons',
-                // // thumbnailImageUrl: buttonsImageURL,
-                // title: 'My button sample',
-                // text: 'Hello, my button',
-                // actions: [
-                //   { label: '中央氣象局', type: 'uri', uri: 'https://www.cwb.gov.tw/V7/forecast/' },
-                //   { label: '台北', type: 'message', text: '臺北天氣' },
-                //   { label: '嘉義', type: 'postback', data: '嘉義天氣' }
-                //   // { label: 'Say hello1', type: 'postback', data: 'hello こんにちは' },
-                //   // { label: '言 hello2', type: 'postback', data: 'hello こんにちは', text: 'hello こんにちは' },
-                // ],
-                type: 'carousel',
-                columns: [
-                  {
-                    thumbnailImageUrl: buttonsImageURL_N,
-                    imageSize: 'contain',
-                    imageAspectRatio: 'rectangle',
-                    title: '氣象資訊',
-                    text: '北部',
-                    actions: [
-                      { label: '中央氣象局', type: 'uri', uri: 'https://www.cwb.gov.tw/V7/forecast/' },
-                      { label: '臺北', type: 'message', text: '臺北天氣' },
-                      { label: '宜蘭', type: 'message', text: '宜蘭天氣' },
-                      // { label: '基隆', type: 'message', text: '基隆天氣' },
-                    ],
-                  },
-                  {
-                    thumbnailImageUrl: buttonsImageURL_M,
-                    imageSize: 'contain',
-                    imageAspectRatio: 'rectangle',
-                    title: '氣象資訊',
-                    text: '中南部',
-                    actions: [
-                      { label: '臺中', type: 'message', text: '臺中天氣' },
-                      { label: '玉山', type: 'message', text: '玉山天氣' },
-                      { label: '日月潭', type: 'message', text: '日月潭天氣' },
-                    ]
-                  },
-                  {
-                    thumbnailImageUrl: buttonsImageURL_S,
-                    imageSize: 'contain',
-                    imageAspectRatio: 'rectangle',
-                    title: '氣象資訊',
-                    text: '中南部',
-                    actions: [
-                      { label: '嘉義', type: 'message', text: '嘉義天氣' },
-                      { label: '臺南', type: 'message', text: '臺南天氣' },
-                      { label: '高雄', type: 'message', text: '高雄天氣' },
-                      // { label: '屏東', type: 'message', text: '屏東天氣' },
-                    ]
-                  },
-                  {
-                    thumbnailImageUrl: buttonsImageURL_E,
-                    imageSize: 'contain',
-                    imageAspectRatio: 'rectangle',
-                    title: '氣象資訊',
-                    text: '東部',
-                    actions: [
-                      { label: '花蓮', type: 'message', text: '花蓮天氣' },
-                      { label: '臺東', type: 'message', text: '臺東天氣' },
-                      { label: '蘭嶼', type: 'message', text: '蘭嶼天氣' }
-                    ]
-                  },
-                  {
-                    thumbnailImageUrl: buttonsImageURL_F,
-                    imageSize: 'contain',
-                    imageAspectRatio: 'rectangle',
-                    title: '氣象資訊',
-                    text: '外島',
-                    actions: [
-                      { label: '澎湖', type: 'message', text: '澎湖天氣' },
-                      { label: '金門', type: 'message', text: '金門天氣' },
-                      { label: '馬祖', type: 'message', text: '馬祖天氣' }
+    if (requestMessage == "注意事項") {
+      bot.push(lineid, "1.注意掉落物\n2.留意腳邊障礙物\n3.配戴安全帽、安全護目鏡及安全手套\n4.物品不可任意堆置、通道要保持流通");
+      console.log(user);
+    }
+    else if (requestMessage == "當前人數") {
+      bot.push(lineid, "目前有" + (people_num) + "人");
+    }
+    else if (requestMessage == "我要註冊") {
 
-                    ]
-                  },
+      let checkuser = fireBaseCollector.getUserByLineId(lineid);
+      if (checkuser) {
+        bot.push(lineid, "您已註冊過,ID：" + (user));
+      }
+      else {
+        bot.push(lineid, "您已成功註冊!\nID:" + user + "\n註冊ID：" + lineid);
+        fireBaseCollector.addUser(lineid, user);
+      }
+      console.log(fireBaseCollector.getUserByLineId(lineid));
+    }
+    else if (requestMessage == "天氣資訊") {
+      bot.push(
+        lineid,
+        {
+          type: 'template',
+          altText: '天氣資訊清單',
+          template: {
+            type: 'carousel',
+            columns: [
+              {
+                thumbnailImageUrl: buttonsImageURL_N,
+                imageSize: 'contain',
+                imageAspectRatio: 'rectangle',
+                title: '氣象資訊',
+                text: '北部',
+                actions: [
+                  { label: '中央氣象局', type: 'uri', uri: 'https://www.cwb.gov.tw/V7/forecast/' },
+                  { label: '臺北', type: 'message', text: '臺北天氣' },
+                  { label: '宜蘭', type: 'message', text: '宜蘭天氣' },
                 ],
               },
-            }
-          );
-        }
-        // else if 
-        // event.source.profile().then(function(profile){
-        //  else if(event.message.type == 'text'){	
+              {
+                thumbnailImageUrl: buttonsImageURL_M,
+                imageSize: 'contain',
+                imageAspectRatio: 'rectangle',
+                title: '氣象資訊',
+                text: '中南部',
+                actions: [
+                  { label: '臺中', type: 'message', text: '臺中天氣' },
+                  { label: '玉山', type: 'message', text: '玉山天氣' },
+                  { label: '日月潭', type: 'message', text: '日月潭天氣' },
+                ]
+              },
+              {
+                thumbnailImageUrl: buttonsImageURL_S,
+                imageSize: 'contain',
+                imageAspectRatio: 'rectangle',
+                title: '氣象資訊',
+                text: '中南部',
+                actions: [
+                  { label: '嘉義', type: 'message', text: '嘉義天氣' },
+                  { label: '臺南', type: 'message', text: '臺南天氣' },
+                  { label: '高雄', type: 'message', text: '高雄天氣' },
+                ]
+              },
+              {
+                thumbnailImageUrl: buttonsImageURL_E,
+                imageSize: 'contain',
+                imageAspectRatio: 'rectangle',
+                title: '氣象資訊',
+                text: '東部',
+                actions: [
+                  { label: '花蓮', type: 'message', text: '花蓮天氣' },
+                  { label: '臺東', type: 'message', text: '臺東天氣' },
+                  { label: '蘭嶼', type: 'message', text: '蘭嶼天氣' }
+                ]
+              },
+              {
+                thumbnailImageUrl: buttonsImageURL_F,
+                imageSize: 'contain',
+                imageAspectRatio: 'rectangle',
+                title: '氣象資訊',
+                text: '外島',
+                actions: [
+                  { label: '澎湖', type: 'message', text: '澎湖天氣' },
+                  { label: '金門', type: 'message', text: '金門天氣' },
+                  { label: '馬祖', type: 'message', text: '馬祖天氣' }
 
-        else if (requestMessage.indexOf('天氣') != -1) {
-          wt.forEach(function (e, i) {
-            if (requestMessage.indexOf(e[0]) != -1) {
-              replyMsg = e[0] + ' 天氣' + '\n' + '天氣狀況 : ' + e[1] + '\n' + '溫度 : ' + e[2] + ' °C' + '\n' + '能見度 : ' + e[3] + ' 公里' + '\n' + '海平面氣壓 : ' + e[4] + ' mbar' + '\n' + '風速 : ' + e[5] + ' 公尺/秒' + '\n' + '風向 : ' + e[6] + '\n' + '日累積雨量 : ' + e[7] + ' 毫米' + '\n' + '更新時間 : ' + e[8];
-              event.reply(replyMsg);
-            }
-          });
-          if (replyMsg == '') {
-            replyMsg = '抱歉, 找不到這個地點, 請輸入其它地點';
-            event.reply(replyMsg);
-          }
+                ]
+              },
+            ],
+          },
         }
-
-        // }
-        // else
-        // {
-        //   event.reply({
-        //     type: 'sticker',
-        //     packageId: '2',
-        //     stickerId: getRandomInt(21,47).toString()
-        //   });
-        //     }
-        // });
-        else {
-          bot.push(lineid, "我看不懂你說的[ " + requestMessage + " ]");
+      );
+    }
+    else if (requestMessage.indexOf('天氣') != -1) {
+      wt.forEach(function (e, i) {
+        if (requestMessage.indexOf(e[0]) != -1) {
+          replyMsg = e[0] + ' 天氣' + '\n' + '天氣狀況 : ' + e[1] + '\n' + '溫度 : ' + e[2] + ' °C' + '\n' + '能見度 : ' + e[3] + ' 公里' + '\n' + '海平面氣壓 : ' + e[4] + ' mbar' + '\n' + '風速 : ' + e[5] + ' 公尺/秒' + '\n' + '風向 : ' + e[6] + '\n' + '日累積雨量 : ' + e[7] + ' 毫米' + '\n' + '更新時間 : ' + e[8];
+          event.reply(replyMsg);
         }
-
+      });
+      if (replyMsg == '') {
+        replyMsg = '抱歉, 找不到這個地點, 請輸入其它地點';
+        event.reply(replyMsg);
       }
-    });
-  });
+    }
+
+    else if (requestMessage == "個人資訊") {
+      _getStatus();
+    }
+    else if (requestMessage == "所在位置") {
+      bot.push(lineid, JustGps);
+    }
+    else {
+      bot.push(lineid, "我看不懂你說的[ " + requestMessage + " ]");
+    }
+
+    
+  });//Loading Firebase message json for reply.
 });
 
 const app = express();
@@ -219,7 +212,6 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/views/index.html');
 });
 app.post('/data', [bodyParser.json(), bodyParser.urlencoded({ extended: false })], function (req, res) {
-  // console.log(req.body);
   let reqJson = req.body;
   if (!reqJson.TYPE) {
     res.status(501).send('Bad Request');
@@ -289,8 +281,6 @@ function broadcast(channel, msg) {
 
 var joinList = [];
 var unknowjoinList = [];
-
-
 
 //Repeat Message
 // setTimeout(function () {
@@ -372,8 +362,6 @@ function _getWeather() {
   });
   timer = setInterval(_getWeather, 3600000);
 }
-
-
 
 function find(arr, s, v) {
   for (let i = 0; i < arr.length; i++) {

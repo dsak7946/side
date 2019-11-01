06145d01f148c701a1f1f890511d1383bf95ed6e
dsak7946 <43715615+dsak7946@users.noreply.github.com>
2019-11-02 01:17:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -341,7 +341,6 @@ else if (requestMessage == "移除使用者"){
   }
   else if (requestMessage == "南港國宅氣爆案") {
     bot.push(lineid,"https://news.ltn.com.tw/news/society/breakingnews/2935656");
-    console.log(news);
   }
   else {
     bot.push(lineid, "我看不懂你說的[ " + requestMessage + " ]");
